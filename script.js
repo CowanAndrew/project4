b45d06471c120c9acefda47fd7429a5071c9e1b0
Andrew Cowan <123470009+CowanAndrew@users.noreply.github.com>
2024-04-08 07:35:28 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+    Author: Andrew Cowan 
+    Date:   
+
+    Filename: project01-04.js
+*/
+
+// Variables for home address
+var homeStreet = "1 Main St.";
+var homeCity = "Sicilia";
+var homeState = "MA";
+var homeCode = "02103";
+
+// Variables for work address
+var workStreet = "15 Oak Ln.";
+var workCity = "Central City";
+var workState = "MA";
+var workCode = "02104";
